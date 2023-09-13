f2df03ca0eadaf41e74c625c6d64132b76da3419
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-09-13 15:09:11 -0300

    fix (1894692128): Bug in reload
    https://github.com/jgb27/abelhinha-pedagogica/issues/10#issue-1894692128
    
    Resolvido, a maneira que o site lida com as rotas foi alterando, deixando ele mais segura e rápido.
    
    Durante a alteração foi visto que não sei tem uma pagina de Not Found, acredito que seja bom criar uma

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Catalogo from './pages/Catalogo'
 import Contato from './pages/Contato'
@@ -7,14 +7,18 @@ import AdminPanel from './pages/AdminPanel'
 import Details from './pages/Details'
 import Login from './pages/Login';
 
-const App = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/catalogo", element: <Catalogo /> },
-  { path: "/contato", element: <Contato /> },
-  { path: "/login", element: <Login /> },
-  { path: "/admin", element: <AdminPanel /> },
-  { path: "/details/:id", element: <Details /> },
-  { path: "*", element: <div><h1>404</h1></div> },
-]);
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/catalogo' element={<Catalogo />} />
+      <Route path='/admin' element={<AdminPanel />} />
+      <Route path='/contato' element={<Contato />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/details/:id' element={<Details />} />
+      <Route path='*' element={<h1>404</h1>} />
+    </Routes>
+  )
+}
 
 export default App;
