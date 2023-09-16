46e52f5b2fd2eaaedbac14b1e72eb365e4d71e60
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-09-16 17:55:53 -0300

    New login UI
    O login ta de cara nova

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,11 +3,56 @@ import {
   Heading,
   VStack,
   Image,
-  Flex
+  HStack,
+  Flex,
+  Avatar,
+  Text
 } from "@chakra-ui/react";
 
 import { NavLink } from "react-router-dom"
 import ThemeToggle from "../ThemeToggle";
+
+const UserInfo = () => {
+  const token = localStorage.getItem('token')
+  return (
+    token
+      ? (<HStack spacing={{ base: '0', md: '6' }} position='absolute' left={[3, 8]} >
+        <Flex alignItems={'center'} textAlign='center'>
+          <VStack
+            display={{ base: 'none', md: 'flex' }}
+            alignItems="flex-start"
+            spacing="1px"
+            ml="2">
+            <Text fontSize="sm">
+              Logado
+            </Text>
+            <Text fontSize="xs" color="gray.600">
+              não logado
+            </Text>
+          </VStack>
+        </Flex>
+      </HStack>)
+      :
+      <Box position="absolute" left={[3, 8]}>
+        <NavLink to="/login" textDecoration="none">
+          <Text
+            color="white"
+            bg="blue.500"
+            padding="8px 16px"
+            borderRadius="4px"
+            fontSize="18px"
+            fontWeight="bold"
+            _hover={{
+              bg: "green.400",
+              color: "white",
+            }}
+          >
+            Acesse sua conta
+          </Text>
+        </NavLink>
+      </Box>
+  )
+}
 
 const Header = ({ title, image }) => {
 
@@ -21,7 +66,8 @@ const Header = ({ title, image }) => {
       pt="1rem"
       pb="0.5rem"
     >
-      <ThemeToggle possition='absolute' spacing={[3, 8]} />
+      <ThemeToggle position='absolute' spacing={[3, 8]} />
+      <UserInfo />
       <Flex as="div" align="center" justify="center" gap="0.5rem">
         <Image src={image} alt="abelhinha" w="2rem" />
         <Heading as="h1" fontSize="1.5rem">
@@ -39,7 +85,6 @@ const Header = ({ title, image }) => {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/catalogo">Catalogo</NavLink>
         <NavLink to="/contato">Contato</NavLink>
-        <NavLink to="/login">Access</NavLink>
       </Box>
     </VStack>
   );
